b7248d62f8be4560c2ac39c5a35b9d99c10b8c84
dborione <db1997@outlook.fr>
2023-09-09 12:18:52 +0200

    separate ft for join

--- src/initialize.c
+++ src/initialize.c
@@ -1,37 +1,16 @@
 #include "../includes/philo.h"
 
-void ft_join(t_philo *philo)
+void    ft_join_threads(t_table *t)
 {
-    // if (pthread_join(philo->thread, NULL) != 0)
-    //     exit(0);
-    printf("philo %d is joined\n", philo->id);
-}
-
-void ft_join_philo(t_table *table, t_philo **philo)
-{
-    int i ;
+    int i;
 
     i = 0;
-    while (i < table->philo_nbr)
+    while (i < t->philo_nbr)
     {
-        //ft_join(philo[i]);
-        printf("%d\n", philo[i]->id);
+        if (pthread_join(t->philo[i].thread, NULL) != 0)
+            exit(2);
         i++;
     }
-}
-
-void    ft_create_philo(int i, t_philo *philo)
-{
-    philo = malloc(sizeof(*philo));
-    // if (!philo)
-        // ft_free_and_exit();
-    philo->id = i + 1;
-    philo->status = IDLE;
-    //printf("%d\n", philo->id);
-
-    // if (pthread_join(philo->thread, NULL) != 0)
-    //     exit(0);   
-
 }
 
 void    ft_init_philos(t_table *t)
@@ -47,23 +26,17 @@ void    ft_init_philos(t_table *t)
             exit(3);
         i++;
     }
-    i = 0;
-    while (i < t->philo_nbr)
-    {
-        if (pthread_join(t->philo[i].thread, NULL) != 0)
-            exit(0);
-        i++;
-    }
-
 }
 
 void    ft_init_table(char **argv, t_table *t)
 {
-    t->philo_nbr = ft_atoi(argv[1]);
+    t->philo_nbr = ft_atoi(argv[1]); //protec
     t->time_to_die = 0;
     t->time_to_eat = 0;
     t->time_to_sleep = 0;
     t->philo = malloc(sizeof(*t->philo) * t->philo_nbr); //protec
+    if (!t->philo)
+        exit(6);
 }
 
 void    ft_init_sim(char **argv)
@@ -72,5 +45,5 @@ void    ft_init_sim(char **argv)
 
     ft_init_table(argv, &t);
     ft_init_philos(&t);
-    //ft_join_philo(&table, &philo);
+    ft_join_threads(&t);
 }
--- src/main.c
+++ src/main.c
@@ -12,35 +12,10 @@ void    *ft_routine(void *philo)
 //         printf(" idle\n");
 }
 
-// void    ft_destroy_threads(t_table *table, t_philo *philo)
-// {
-//     int i;
-    
-//     i = 0;
-//     while (philo[i])
-//     {
-//         if (pthread_join(philo->thread, NULL) != 0) {
-//             //return 2;
-//             exit(0);
-//         }
-//         i++;
-//         printf("Thread %d has ended\n", philo->id);
-//     }
-// }
-
 
 int main(int argc, char **argv)
 {
-    //t_philo philo;
-    //pthread_mutex_t  *forks;
-
-    //check input args
-    //initialize table
-    //launch threads
+    // ft_check args
     ft_init_sim(argv);
-   // ft_join_threads();
-    //ft_create_threads(&table, &philo);
-    //ft_destroy_threads(&table, &philo);
-
     return (0);
 }
